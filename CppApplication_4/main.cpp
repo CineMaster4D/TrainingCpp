2649ecf0e20103e4b63ecd53e7cd652ad43d3506
unknown <xodsergey@DUKENT16.QUVVAT.local>
2018-09-21 09:20:17 +0500

    add CppApplication_4/

---
+++ CppApplication_4/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: xodsergey
+ *
+ * Created on 20 сентября 2018 г., 13:00
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
