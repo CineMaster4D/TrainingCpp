80eecc7da935d4beb1f3ea1e2c48b9e8d3235957
unknown <xodsergey@DUKENT16.QUVVAT.local>
2018-09-21 09:28:56 +0500

    add CppApplication_7/

---
+++ CppApplication_7/main.c
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: xodsergey
+ *
+ * Created on 20 сентября 2018 г., 15:35
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+
+main()
+{
+    double count;
+    for (count = 0; getchar != EOF; ++count)
+    
+        ;
+        printf("%0F\n", count);
+    
+}
+
